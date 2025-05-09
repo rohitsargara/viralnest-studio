173708cf1e5dad8a73e280b0ebc49d6d4ac015e7
Rohit <Viralneststudio1@gmail.com>
2025-05-09 19:43:25 +0530

    Slider

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Business.css";
 
 // Separate images for each slider
@@ -14,33 +14,19 @@ import imgBottom3 from "../assets/desktopslider7.jpg";
 import imgBottom4 from "../assets/desktopslider8.jpg";
 import imgBottom5 from "../assets/desktopslider9.jpg";
 
-// Define arrays for each slider
 const imagesTop = [imgTop1, imgTop2, imgTop3, imgTop4, imgTop5];
 const imagesBottom = [imgBottom1, imgBottom2, imgBottom3, imgBottom4, imgBottom5];
 
 const Slider = ({ reverse, images }) => {
-  const [offset, setOffset] = useState(0);
-
-  useEffect(() => {
-    let animationFrame;
-    const moveSlider = () => {
-      setOffset((prev) => prev + 0.5);
-      animationFrame = requestAnimationFrame(moveSlider);
-    };
-    moveSlider();
-    return () => cancelAnimationFrame(animationFrame);
-  }, []);
-
   return (
-    <div
-      className={`slider-track ${reverse ? "reverse" : ""}`}
-      style={{ transform: `translateX(${reverse ? offset : -offset}px)` }}
-    >
-      {[...images, ...images].map((img, index) => (
-        <div className="slide" key={index}>
-          <img src={img} alt={`slide-${index}`} />
-        </div>
-      ))}
+    <div className={`slider-track-wrapper ${reverse ? "reverse" : ""}`}>
+      <div className="slider-track">
+        {[...images, ...images].map((img, index) => (
+          <div className="slide" key={index}>
+            <img src={img} alt={`slide-${index}`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
@@ -50,7 +36,6 @@ const Business = () => {
     <div className="business-section">
       <div className="overlay"></div>
 
-      {/* Text Section */}
       <div className="text-container">
         <h1 className="title">
           Work That Speaks <span className="highlight">For Itself</span>
@@ -61,10 +46,9 @@ const Business = () => {
         </p>
       </div>
 
-      {/* Sliders */}
       <div className="slider-wrapper">
-        <Slider reverse={false} images={imagesTop} />   {/* Top slider */}
-        <Slider reverse={true} images={imagesBottom} /> {/* Bottom slider */}
+        <Slider reverse={false} images={imagesTop} />
+        <Slider reverse={true} images={imagesBottom} />
       </div>
     </div>
   );
